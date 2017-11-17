8d1f25f1187e6260e0f21f1d185cb68dc2b2f191
Christophe Michel <christopheml@users.noreply.github.com>
2017-11-17 07:43:01 +0100

    Improved test coverage of name matchers and put test cases in the right test classes.

--- src/test/java/com/github/christopheml/mtgapi/requests/NameStartsWithMatcherTest.java
+++ src/test/java/com/github/christopheml/mtgapi/requests/NameStartsWithMatcherTest.java
@@ -7,24 +7,24 @@ import java.util.List;
 
 import static com.github.christopheml.mtgapi.requests.CardQuery.query;
 import static com.github.christopheml.mtgapi.requests.NameMatcher.equalsTo;
+import static com.github.christopheml.mtgapi.requests.NameMatcher.startsWith;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CardQueryTest {
+public class NameStartsWithMatcherTest {
 
     @Test
-    public void test_name_parameter() throws Exception {
-        // FIXME This test is probably out of place here
+    public void url_parameter_should_be_set() throws Exception {
         String url = query().name(equalsTo("Darkness")).toUrl("https://api.magicthegathering.io/v1/cards");
         assertThat(url).isEqualTo("https://api.magicthegathering.io/v1/cards?name=Darkness");
     }
 
     @Test
-    public void test_name_result_filter() throws Exception {
-        CardQuery cardQuery = query().name(equalsTo("Darkness"));
-        List<Card> cards = asList(cardWithName("Dark Ritual"), cardWithName("Lightning Bolt"), cardWithName("Darkness"));
+    public void test_result_filters() throws Exception {
+        CardQuery cardQuery = query().name(startsWith("Darkness"));
+        List<Card> cards = asList(cardWithName("Dark Ritual"), cardWithName("Grasp of Darkness"), cardWithName("Lightning Bolt"), cardWithName("Darkness"));
         List<Card> filteredCards = cardQuery.filter(cards);
-        assertThat(filteredCards).extracting(Card::getName).containsExactly("Darkness");
+        assertThat(filteredCards).extracting(Card::getName).containsOnly("Darkness");
     }
 
     private Card cardWithName(String name) {
